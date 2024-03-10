5899dd485ac3c6cf9f941f47a363b6c0c4c54fcc
JTH <jangthanhhai@gmail.com>
2024-03-10 10:02:19 +0000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Phepnhan
+
+Một thư viện giúp bạn nhân 2 số bất kỳ.
+Đơn giản, dễ dùng, cực kỳ nhẹ
---
+++ nhan.js
@@ -0,0 +1,4 @@
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+const mul = (a, b) => a * b
+exports.mul = mul
